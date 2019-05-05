b13c604ef43d6a12e59cb5e27e71905ed155ddbe
okan yüksel <y_okan1995@hotmail.com>
2019-05-05 23:05:55 +0300

    Initial commit

---
+++ app/src/main/java/com/okanyuksel/planets/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.okanyuksel.planets;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+
+public class SplashActivity extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
+    }
+
+}
